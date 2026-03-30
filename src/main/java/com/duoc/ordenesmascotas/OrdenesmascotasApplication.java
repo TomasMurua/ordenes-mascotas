3c5f13eb898dcfe352f8db45e4cc4a04469cf176
TomasMurua <tomaspmurua@gmail.com>
2026-03-30 16:06:15 -0300

    agregar clase principal y gitignore

---
+++ src/main/java/com/duoc/ordenesmascotas/OrdenesmascotasApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.ordenesmascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdenesmascotasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdenesmascotasApplication.class, args);
+    }
+
+}
